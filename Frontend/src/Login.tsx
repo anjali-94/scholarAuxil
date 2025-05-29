138fdb864454d3433f10d65c8cf653e1c4eb0e8d
anjali-94 <anjalishr987@gmail.com>
2025-05-29 16:46:21 +0530

    Revert "Routing-issue-resolved"
    This reverts commit 6b7e0c5f4c530098e9fd359fa71abb3ef087120a.

--- Frontend/src/Login.tsx
+++ Frontend/src/Login.tsx
@@ -37,7 +37,7 @@ const Login = () => {
         signInWithEmailAndPassword(auth, email, password)
             .then((response) => {
                 console.log(response.user.uid);
-                navigate('/app');
+                navigate('/');
             })
             .catch((error) => {
                 console.log(error);
